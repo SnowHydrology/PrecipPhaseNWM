58f76ee976b0f76ac0cc93132c8a7ed9976aae2b
SnowHydrology <ksj.hydro@gmail.com>
2025-04-29 15:03:11 -0400

    inital batch of ppms

---
+++ analysis/step01_partitioning_functions.R
@@ -0,0 +1,74 @@
+# Functions for rain-snow prediction
+
+# Load packages 
+library(tidyverse)
+
+
+#' Single-value threshold for precipitation phase partitioning
+#'
+#' @param temp_degC Input temperature (air, wet bulb, dew point) in degrees Celsius
+#' @param thresh_degC Temperature threshold for rain-snow partitioning in degrees Celsius
+#'
+#' @returns The fraction of solid precipitation where 1 = all snow and 0 = all rain
+#' @export
+#'
+#' @examples phaseThresh(1, 2)
+phaseThresh <- function(temp_degC, thresh_degC){
+  ifelse(temp_degC > thresh_degC,
+         0,
+         1)
+}
+
+#' Dual-threshold range for precipitation phase partitioning
+#'
+#' @param temp_degC Input temperature (air, wet bulb, dew point) in degrees Celsius
+#' @param thresh_low_degC Lower temperature threshold in degrees Celsius (below = all snow, above = mix)
+#' @param thresh_high_degC Upper temperature threshold in degrees Celsius (above = all rain, below = mix)
+#'
+#' @returns The fraction of solid precipitation where 1 = all snow and 0 = all rain
+#' @export
+#'
+#' @examples phaseThresh(1, -1, 3)
+phaseRange <- function(temp_degC, thresh_low_degC, thresh_high_degC){
+  ifelse(temp_degC >= thresh_high_degC,
+         0,
+         ifelse(temp_degC <= thresh_low_degC,
+                1,
+                (temp_degC - thresh_low_degC) / (thresh_high_degC - thresh_low_degC)))
+}
+
+
+
+#' SNTHERM (Jordan, 1990) precipitation phase partitioning method
+#'
+#' @param tair_degC Input air temperature in degrees Celsius
+#'
+#' @returns The fraction of solid precipitation where 1 = all snow and 0 = all rain
+#' @export
+#'
+#' @examples phaseJordan(1)
+phaseJordan <- function(tair_degC){
+  ifelse(tair_degC > 2.5,
+         0,
+         ifelse(tair_degC <= 0.5,
+                1,
+                ifelse(tair_degC <= 2,
+                       1 - (-54.632 + 0.2 * (tair_degC + 273.15)),
+                       0.6)))
+}
+
+#' Binary logistic regression model (bivariate) for precipitation phase partitioning
+#'
+#' @param tair_degC Input air temperature in degrees Celsius
+#' @param rh_pct Input relative humidity in percent
+#'
+#' @returns The fraction of solid precipitation where 1 = all snow and 0 = all rain
+#' @export
+#'
+#' @examples phaseBinlog(1, 80)
+phaseBinlog <- function(tair_degC, rh_pct){
+  snow_prob = 1/(1 + exp(-10.04 + 1.41 * tair_degC + 0.09 * rh_pct))
+  ifelse(snow_prob > 0.5,
+         1,
+         0)
+}
